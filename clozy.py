20f32c1213d29582ea83b726db10a0df16373798
Eyal Dolev <eyalliron.dolev@uzh.ch>
2021-04-07 15:30:36 +0200

    Some commit

--- clozy.py
+++ clozy.py
@@ -1,7 +1,9 @@
 import spacy
-from typing import Tuple
+import re
+from typing import Tuple, List
 
 nlp = spacy.load('de_core_news_sm')
+adjective_suffixes = {'er', 'e', 'es', 'em', 'en'}
 
 
 def erase_token(token, schuettelbox):
@@ -11,44 +13,58 @@ def erase_token(token, schuettelbox):
 def enumerate_blanks():
     pass
 
+def detokenizer(line: List[str]) -> str:
+    return "".join(' ' + token if token.isalpha() else token for token in line)[1:]
+
 
 def remove_pos(file, tags):
     schuettelbox = []
+    return_line = []
     with open(file, 'r', encoding='utf-8') as f:
         for line in f:
-            for ctr, token in enumerate(nlp(line)):
-                print(ctr, token)
+            for token in nlp(line):
                 if token.pos_ in tags:
-                    a = erase_token()
-                    return_line.append(erase_token(token.text, schuettelbox)[0])
-                    schuettelbox.append(erase_token(token.text, schuettelbox)[1])
+                    blank = erase_token(token.text, schuettelbox)
+                    return_line.append(blank[0])
+                    schuettelbox.append(blank[1])
                 else:
                     return_line.append(token.text)
-    return "".join(' ' + token if token.isalpha() else token for token in return_line), schuettelbox
+    return detokenizer(return_line), schuettelbox
 
 
 file = 'sample_texts/text1.txt'
 n = 10
-return_line = []
-# with open('sample_texts/text1.txt', 'r', encoding='utf-8') as file:
-#     token_ctr = 0
-#     for line in file:
-#         for token in nlp(line):
-#             token_ctr += 1
-#             if token_ctr == n:
-#                 token_ctr = 0
-#                 return_line.append(erase_token(token.text)[0])
-#                 schuettelbox.append(erase_token(token.text)[1])
-#             else:
-#                 return_line.append(token.text)
 
+
+def remove_nth_word(file, n):
+    return_line = []
+    schuettelbox = []
+    with open('sample_texts/text1.txt', 'r', encoding='utf-8') as file:
+        token_ctr = 0
+        for line in file:
+            for token in nlp(line):
+                token_ctr += 1
+                if token_ctr == n:
+                    token_ctr = 0
+                    blank = erase_token(token.text, schuettelbox)
+                    return_line.append(blank[0])
+                    schuettelbox.append(blank[1])
+                else:
+                    return_line.append(token.text)
+    return detokenizer(return_line), schuettelbox
+
+
+def remove_adjective_suffix(token):
+    return re.sub(r'(es|er|em|en|e)$', '____', token)
 
 
 
 tags = ['NOUN']
 
+# return_line, schuettelbox = remove_pos(file, tags)
+# print(return_line)
+# print(schuettelbox)
 
-return_line, schuettelbox = remove_pos(file, tags)
-print(return_line)
-print(schuettelbox)
-
+# cloze_test, schuettelbox = remove_nth_word(file, n)
+# print (cloze_test)
+# print(schuettelbox)
--- test_clozy.py
+++ test_clozy.py
@@ -3,7 +3,24 @@ import clozy
 
 class ClozyTest(TestCase):
 
-    def test_erase_token(self):
-        token = 'Baum'
-        result = clozy.erase_token(token)
-        self.assertEqual( ('_________', 'Baum'), result, "erase_token doesn't work properly")
+    # def test_erase_token(self):
+    #     token = 'Baum'
+    #     result = clozy.erase_token(token)
+    #     self.assertEqual( ('_________', 'Baum'), result, "erase_token doesn't work properly")
+
+    def test_remove_adjective_suffix(self):
+        token = 'schöner'
+        result = clozy.remove_adjective_suffix(token)
+        self.assertEqual('schön____', result, 'adjective suffix doesnt work')
+        token = 'schönes'
+        result = clozy.remove_adjective_suffix(token)
+        self.assertEqual('schön____', result, 'adjective suffix doesnt work')
+        token = 'schönem'
+        result = clozy.remove_adjective_suffix(token)
+        self.assertEqual('schön____', result, 'adjective suffix doesnt work')
+        token = 'schönen'
+        result = clozy.remove_adjective_suffix(token)
+        self.assertEqual('schön____', result, 'adjective suffix doesnt work')
+        token = 'schöne'
+        result = clozy.remove_adjective_suffix(token)
+        self.assertEqual('schön____', result, 'adjective suffix doesnt work')
