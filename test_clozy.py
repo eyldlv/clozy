23e7dbe39c3eccf294b769e0a2a35352f4c5f72c
Eyal Dolev <eyalliron.dolev@uzh.ch>
2021-04-07 21:18:41 +0200

    Add tests

--- test_clozy.py
+++ test_clozy.py
@@ -3,24 +3,34 @@ import clozy
 
 class ClozyTest(TestCase):
 
-    # def test_erase_token(self):
-    #     token = 'Baum'
-    #     result = clozy.erase_token(token)
-    #     self.assertEqual( ('_________', 'Baum'), result, "erase_token doesn't work properly")
+    def test_erase_token(self):
+        
+        token = 'Baum'
+        result = clozy.erase_token(token, [])
+        self.assertEqual( (' (1)_________', 'Baum'), result, "erase_token doesn't work properly")
 
     def test_remove_adjective_suffix(self):
         token = 'schöner'
-        result = clozy.remove_adjective_suffix(token)
+        result = clozy.remove_adjective_suffix_from_token(token)
         self.assertEqual('schön____', result, 'adjective suffix doesnt work')
         token = 'schönes'
-        result = clozy.remove_adjective_suffix(token)
+        result = clozy.remove_adjective_suffix_from_token(token)
         self.assertEqual('schön____', result, 'adjective suffix doesnt work')
         token = 'schönem'
-        result = clozy.remove_adjective_suffix(token)
+        result = clozy.remove_adjective_suffix_from_token(token)
         self.assertEqual('schön____', result, 'adjective suffix doesnt work')
         token = 'schönen'
-        result = clozy.remove_adjective_suffix(token)
+        result = clozy.remove_adjective_suffix_from_token(token)
         self.assertEqual('schön____', result, 'adjective suffix doesnt work')
         token = 'schöne'
-        result = clozy.remove_adjective_suffix(token)
+        result = clozy.remove_adjective_suffix_from_token(token)
         self.assertEqual('schön____', result, 'adjective suffix doesnt work')
+
+    def test_nth_word(self):
+        text = 'Auf einem Baum saß ein alter Hahn.'
+        result = clozy.remove_nth_word(text, 4)[0]
+        self.assertEqual('Auf einem Baum (1)_______ ein alter Hahn.', result)
+
+        text = 'Ich will. Nach Hause.'
+        result = clozy.remove_nth_word(text, 3)[0]
+        self.assertEqual('Ich will. (1)_________ Hause.', result, 'If word is a punctuation mark or a number it should be skipped.')
