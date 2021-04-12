1b617b2e966e3c0f54b01a6a5326d0dee974a80b
Eyal Dolev <eyalliron.dolev@uzh.ch>
2021-04-12 17:36:08 +0200

    Streamline functions

--- clozy.py
+++ clozy.py
@@ -7,9 +7,10 @@ nlp = spacy.load('de_core_news_sm')
 adjective_suffixes = {'er', 'e', 'es', 'em', 'en'}
 
 
-def erase_token(token, schuettelbox):
+def erase_token(token:str, schuettelbox:List[str]) -> str:
+    """ Return a tuple containing the blank"""
     retr_string = ' (' + str(len(schuettelbox)+1) + ')' + '_' * (len(token)*2 +1) 
-    return (retr_string, token)
+    return retr_string
 
 
 def get_positions_of_pos(doc, tags):
@@ -24,21 +25,6 @@ def remove_adjective_suffix_from_token(token):
     return re.sub(r'(es|er|em|en|e)$', '____', token)
 
 
-# def remove_pos(file, tags):
-#     schuettelbox = []
-#     return_line = []
-#     with open(file, 'r', encoding='utf-8') as f:
-#         for line in f:
-#             for token in nlp(line):
-#                 if token.pos_ in tags:
-#                     blank = erase_token(token.text, schuettelbox)
-#                     return_line.append(blank[0])
-#                     schuettelbox.append(blank[1])
-#                 else:
-#                     return_line.append(token.text)
-#     return detokenizer(return_line), schuettelbox
-
-
 file = 'sample_texts/text1.txt'
 n = 10
 
@@ -50,9 +36,8 @@ def remove_nth_word(text:List[spacy.tokens.Token], schuettelbox:List[str], n:int
         token_ctr += 1
         if token_ctr >= n and token.is_alpha:
             token_ctr = 0
-            blank = erase_token(token.text, schuettelbox)
-            return_string.append(blank[0])
-            schuettelbox.append(blank[1])
+            return_string.append(erase_token(token.text, schuettelbox))
+            schuettelbox.append(token.text)
         else:
             return_string.append(token.text)
     return detokenizer(return_string), schuettelbox
@@ -69,9 +54,8 @@ def remove_pos_new(text:List[spacy.tokens.Token], schuettelbox:List[str], tags:L
     random_positions = [positions.pop(randint(0,len(positions)-1)) for _ in range(round(len(positions)*percentage))]
     for position, token in enumerate(text):
         if position in random_positions:
-            blank = erase_token(token.text, schuettelbox)
-            return_string.append(blank[0])
-            schuettelbox.append(blank[1])
+            return_string.append(erase_token(token.text, schuettelbox))
+            schuettelbox.append(token.text)
         else:
             return_string.append(token.text)
     return detokenizer(return_string), schuettelbox
@@ -83,14 +67,6 @@ def check(text):
 
 tags = ['NOUN']
 
-# print(remove_pos_new(nlp('Die Katze sitzt auf dem Tisch.')))
-# return_line, schuettelbox = remove_pos(file, tags)
-# print(return_line)
-# print(schuettelbox)
-
-# cloze_test, schuettelbox = remove_nth_word(file, n)
-# print (cloze_test)
-# print(schuettelbox)
 
 schuettelbox = []
 blank_text = []
@@ -120,6 +96,6 @@ for paragraph in blank_text:
     print(paragraph)
 print(schuettelbox)
 
-check(text)
+# check(text)
 
 # NOUN, VERB, AUX, ADJ, ADV (noch, jedoch, schon, selten), ADP (präpositionen), DET, SCONJ (dass), CCONJ (und, aber)
