d173f7be9dc264520724b7e29629c0570b9db397
Eyal Dolev <eyalliron.dolev@uzh.ch>
2021-05-26 21:06:28 +0200

    Fix tests

--- test_clozy.py
+++ test_clozy.py
@@ -1,8 +1,8 @@
 from unittest import TestCase, main
 import clozy
 import spacy
+from constants import nlp
 
-nlp = spacy.load('de_core_news_sm')
 
 class ClozyTest(TestCase):
 
@@ -11,46 +11,45 @@ class ClozyTest(TestCase):
         result = clozy.get_postags(sentence)
         self.assertEqual({'NOUN' : ['Äpfel', 'Bananen'],
                           'AUX' : ['sind'],
-                          'ADV'  : ['lecker', 'gelb'],
+                          'ADJ'  : ['lecker', 'gelb'],
                           'CCONJ' : ['aber']}, result)
 
     def test_erase_token(self):
-        
         token = 'Baum'
-        result = clozy.erase_token(token, [])
+        result = clozy.erase_token(token, 1)
         self.assertEqual( (' (1)_________'), result, "erase_token doesn't work properly")
 
 
     def test_remove_adja_suffix(self):
         token = 'schöner'
-        result = clozy.remove_adja_suffix(token, [])
+        result = clozy.remove_adja_suffix(token, 1)
         self.assertEqual(' (1) schön____', result)
         token = 'schöne'
-        result = clozy.remove_adja_suffix(token, [])
+        result = clozy.remove_adja_suffix(token, 1)
         self.assertEqual(' (1) schön____', result)
         token = 'schönes'
-        result = clozy.remove_adja_suffix(token, [])
+        result = clozy.remove_adja_suffix(token, 1)
         self.assertEqual(' (1) schön____', result)
         token = 'schönem'
-        result = clozy.remove_adja_suffix(token, [])
+        result = clozy.remove_adja_suffix(token, 1)
         self.assertEqual(' (1) schön____', result)
         token = 'schönen'
-        result = clozy.remove_adja_suffix(token, [])
+        result = clozy.remove_adja_suffix(token, 1)
         self.assertEqual(' (1) schön____', result)
     
     def test_add_blank_to_adjd(self):
         token = 'schön'
-        result = clozy.add_blank_to_adjd(token, [])
+        result = clozy.add_blank_to_adjd(token, 1)
         self.assertEqual(' (1) schön____', result)
 
 
     def test_nth_word(self):
         text = nlp('Auf einem Baum saß ein alter Hahn.')
-        result = clozy.nth_word_remover(text, [], 4)
+        result = clozy.nth_word_remover(text, 4)
         self.assertEqual(('Auf einem Baum (1)_______ ein alter Hahn.', ['saß']), result)
 
         text = nlp('Ich will. Nach Hause.')
-        result = clozy.nth_word_remover(text, [], 3)
+        result = clozy.nth_word_remover(text, 3)
         self.assertEqual(('Ich will. (1)_________ Hause.', ['Nach']), result, 'If word is a punctuation mark or a number it should be skipped.')
     
     def test_get_positions_of_pos(self):
@@ -61,11 +60,11 @@ class ClozyTest(TestCase):
     
     def test_pos_new(self):
         text = nlp('Auf einem Baum saß ein alter Hahn.')
-        result = clozy.pos_remover(text, [])
+        result = clozy.pos_remover(text, ['NOUN'])
         self.assertEqual(('Auf einem (1)_________ saß ein alter (2)_________.', ['Baum', 'Hahn']), result)
         
         # Check that only half of the words gets deleted
         text = nlp('Die Katze sitzt auf dem Tisch.')
-        result = clozy.pos_remover(text, [], ['NOUN'], 0.5)[0]
+        result = clozy.pos_remover(text, ['NOUN'], 0.5)[0]
         possible_results = ['Die (1)___________ sitzt auf dem Tisch.', 'Die Katze sitzt auf dem (1)___________.']
         self.assertTrue(result in possible_results)
