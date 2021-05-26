7fb76c12cc088d8d15f0ed9195308fc0d2bbe69e
Eyal Dolev <eyalliron.dolev@uzh.ch>
2021-05-26 17:11:56 +0200

    Add docstrings and type hints

--- clozy.py
+++ clozy.py
@@ -4,33 +4,41 @@ from typing import Tuple, List
 from random import randint
 
 
+
 def erase_token(token:str, gap_num:int) -> str:
-    """ Return a tuple containing the blank"""
+    """ Return blank based on token 
+    
+    args: original token, gap number in the cloze
+    for args token="Baum", gap_num=1  returns: "(1)________"
+    """
     return ' (' + str(gap_num) + ')' + '_' * (len(token)*2 +1) 
 
 
-def get_positions_of_pos(doc, tags):
+def get_positions_of_pos(doc:List[spacy.tokens.Token], tags:List[str]) -> List[int]:
+    """ Get the positions of given postags in the text. Required to remove only part of them."""
     return [i for i in range(len(doc)) if doc[i].pos_ in tags]
 
 
 def detokenizer(line: List[str]) -> str:
+    """ Concetante list of tokens to one string """
     return "".join(' ' + token if token.isalnum() else token for token in line)[1:]
 
 
-def remove_adja_suffix(token, gap_num:int):
-    """Remove suffixes from declined adjectives."""
+def remove_adja_suffix(token:str, gap_num:int) -> str:
+    """Remove suffixes from declined adjectives and return as enumerated blank """
     return ' (' + str(gap_num) + ') ' + re.sub(r'(es|er|em|en|e)$', '____', token) 
 
 
-def add_blank_to_adjd(token, gap_num:int):
+def add_blank_to_adjd(token:str, gap_num:int) -> str:
+    """Remove suffixes from predicate adjectives and return as enumerated blank """
     return ' (' + str(gap_num) + ') ' + token + '____'
 
 
-def nth_word_remover(text:List[spacy.tokens.Token], n:int=10) -> Tuple[str, List[str]]:
+def nth_word_remover(doc:List[spacy.tokens.Token], n:int=10) -> Tuple[str, List[str]]:
     return_string = []
     token_ctr = 0
     schuettelbox = []
-    for token in text:
+    for token in doc:
         token_ctr += 1
         if token_ctr >= n and token.is_alpha:
             token_ctr = 0
@@ -41,17 +49,17 @@ def nth_word_remover(text:List[spacy.tokens.Token], n:int=10) -> Tuple[str, List
     return detokenizer(return_string), schuettelbox
 
 
-def pos_remover(text:List[spacy.tokens.Token], tags:List[str]=['NOUN'], percentage:int=1) -> Tuple[str, List[str]]:
+def pos_remover(doc:List[spacy.tokens.Token], tags:List[str]=['NOUN'], percentage:int=1) -> Tuple[str, List[str]]:
     """
-    Turn a certain pos tag in the text to blanks. Nouns by default. All words of the same pos will be removed unless a diffrent percentage is given.
+    Turn a certain postag in the text to blanks. Nouns by default. All words of the same pos will be removed unless a diffrent percentage is given.
 
     Return a tuple containing the blanked text and a list of strings with the removed tokens.
     """
     schuettelbox = []
     return_string = []
-    positions = get_positions_of_pos(text, tags)
+    positions = get_positions_of_pos(doc, tags)
     random_positions = [positions.pop(randint(0,len(positions)-1)) for _ in range(round(len(positions)*percentage))]
-    for position, token in enumerate(text):
+    for position, token in enumerate(doc):
         if position in random_positions:
             return_string.append(erase_token(token.text, len(schuettelbox)+1))
             schuettelbox.append(token.text)
@@ -60,10 +68,14 @@ def pos_remover(text:List[spacy.tokens.Token], tags:List[str]=['NOUN'], percenta
     return detokenizer(return_string), schuettelbox
 
 
-def adjective_suffix_remover(text):
+def adjective_suffix_remover(doc:List[spacy.tokens.Token]) -> Tuple[str, List[str]]:
+    """ Remove adjective suffixes from the text. Will also add a blank suffix to undeclined predicate adjectives.
+    
+    Returns a tuple containing the cloze text and a list of strings with the removed tokens.
+    """
     return_string = []
     schuettelbox = []
-    for token in text:
+    for token in doc:
         if token.tag_ == 'ADJD':
             return_string.append(add_blank_to_adjd(token.text, len(schuettelbox)+1))
             schuettelbox.append(token.text)
@@ -75,7 +87,9 @@ def adjective_suffix_remover(text):
     return detokenizer(return_string), schuettelbox
 
 
-def print_schuettelbox(schuettelbox) -> str:
+def print_schuettelbox(schuettelbox:List[str]) -> str:
+    """ Convert the list of removed tokens to a string for printing.
+    """
     return_str = '\nLösung:\n----------------\n'
     return_str += ''.join(f'({position}) {word}\n' for position, word in enumerate(schuettelbox,1))
     return_str += '------------------------------------------------------------'
